25e63f77f20c3c3a59f86eb7c3b723a70df37bb9
happy <simplechord@protonmail.com>
2020-04-29 18:25:12 +0800

    created Japanese page

--- js/constant.js
+++ js/constant.js
@@ -1,6 +1,7 @@
 // chords
 var A7 = "A7";
 var Am = "Am";
+var AmG = "Am/G";
 var Am7 = "Am7";
 var bB = "fB";
 var Bm = "Bm";
@@ -9,15 +10,22 @@ var C7 = "C7";
 var D = "D";
 var D6 = "D6";
 var D7 = "D7";
+var D7Fs = "D7/F#";
 var Dm = "Dm";
 var Dm7 = "Dm7";
+var E7 = "E7";
+var E7Gs = "E7/G#";
 var Em = "Em";
 var EmB = "Em/B";
+var EmG = "Em/G";
+var Em7 = "Em7";
 var F = "F";
+var FmM7 = "FmM7";
 var G = "G";
 var GB = "G/B";
 var G7 = "G7";
 var Gm = "Gm";
+var Gsus4 = "Gsus4";
 
 // others
 var SPACE = "\u3000"; // full width
@@ -55,25 +63,30 @@ var FORMAT =
 
 var LANGUAGES =
 {
-    gequ: "zh"
+    gequ: "zh",
+    uta: "ja"
 };
 
 var DICTIONARY =
 {
     footer:
     {
+        ja: "誠にありがとうございます！",
         zh: "希望您会欢喜以上内容！"
     },
     intro:
     {
+        ja: "（前奏）",
         zh: "（前奏）"
     },
     demo:
     {
+        ja: "弾き語り",
         zh: "示范弹唱"
     },
     outro:
     {
+        ja: "（後奏）",
         zh: "（尾奏）"
     }
 }
